1559cb951fe83f0ad478c742316325caa0be6e22
378978764 <378978764@qq.com>
2017-06-10 19:32:06 +0800

    实现了串口收发

---
+++ main.c
@@ -0,0 +1,136 @@
+
+/*------------------------------------------------------------------*/
+/* --- STC MCU International Limited -------------------------------*/
+/* --- STC 1T Series MCU RC Demo -----------------------------------*/
+/* --- Mobile: (86)13922805190 -------------------------------------*/
+/* --- Fax: 86-0513-55012956,55012947,55012969 ---------------------*/
+/* --- Tel: 86-0513-55012928,55012929,55012966 ---------------------*/
+/* --- Web: www.GXWMCU.com -----------------------------------------*/
+/* --- QQ:  800003751 ----------------------------------------------*/
+/* If you want to use the program or the program referenced in the  */
+/* article, please specify in which data and procedures from STC    */
+/*------------------------------------------------------------------*/
+
+
+#include	"config.h"
+#include	"USART1.h"
+#include	"delay.h"
+#include <stdio.h>
+
+
+/*************	功能说明	**************
+
+双串口全双工中断方式收发通讯程序。
+
+通过PC向MCU发送数据, MCU收到后通过串口把收到的数据原样返回.
+
+******************************************/
+
+/*************	本地常量声明	**************/
+
+
+/*************	本地变量声明	**************/
+
+
+/*************	本地函数声明	**************/
+
+
+
+/*************  外部函数和变量声明 *****************/
+unsigned int time_count,count,sum;
+
+
+/*************  串口1初始化函数 *****************/
+void	UART_config(void)
+{
+	COMx_InitDefine		COMx_InitStructure;					//结构定义
+	COMx_InitStructure.UART_Mode      = UART_8bit_BRTx;		//模式,       UART_ShiftRight,UART_8bit_BRTx,UART_9bit,UART_9bit_BRTx
+	COMx_InitStructure.UART_BRT_Use   = BRT_Timer2;			//使用波特率,   BRT_Timer1, BRT_Timer2 (注意: 串口2固定使用BRT_Timer2)
+	COMx_InitStructure.UART_BaudRate  = 115200ul;			//波特率, 一般 110 ~ 115200
+	COMx_InitStructure.UART_RxEnable  = ENABLE;				//接收允许,   ENABLE或DISABLE
+	COMx_InitStructure.BaudRateDouble = DISABLE;			//波特率加倍, ENABLE或DISABLE
+	COMx_InitStructure.UART_Interrupt = ENABLE;				//中断允许,   ENABLE或DISABLE
+	COMx_InitStructure.UART_Polity    = PolityLow;			//中断优先级, PolityLow,PolityHigh
+	COMx_InitStructure.UART_P_SW      = UART1_SW_P30_P31;	//切换端口,   UART1_SW_P30_P31,UART1_SW_P36_P37,UART1_SW_P16_P17(必须使用内部时钟)
+	COMx_InitStructure.UART_RXD_TXD_Short = DISABLE;		//内部短路RXD与TXD, 做中继, ENABLE,DISABLE
+	USART_Configuration(USART1, &COMx_InitStructure);		//初始化串口1 USART1,USART2
+
+	PrintString1("STC15F2K60S2 UART1 Test Prgramme!\r\n");	//SUART1发送一个字符串
+}
+
+
+
+
+
+void initial()
+{
+	count=0;
+	time_count=0;
+   	sum=0;
+
+	TMOD=0x02; //定时器0工作方式2,8位自动装入计数
+	TH0=0x06;
+	TL0=0x06; //定时器赋初值 计时0.25ms，1000次为0.25s,4000次为一秒
+
+	IT0=1;//  外部中断0工作方式（下降沿有效）
+
+	EA=1;
+	ET0=1;
+	EX0=1;
+	TR0=1; //开中断
+}
+void exter0() interrupt 0
+{
+	count++;
+}
+
+void timer0() interrupt 1
+{
+	time_count++;
+	if(time_count==4000)
+	{
+		sum=count;
+		time_count=0;
+		count=0;
+
+
+	}
+}
+/**********************************************/
+void main()
+{
+	u8	i;
+	char str[25];
+
+	UART_config();
+	EA = 1;
+
+	//PWM初始化
+
+	P0M0=0;
+	P0M1=0;
+	initial();
+
+	while(1){
+		sprintf(str,"%d\n\r",sum);
+		PrintString1(str);
+	}
+	while (1)
+	{
+		delay_ms(2000);
+		if(COM1.RX_TimeOut > 0)		//超时计数
+		{
+			if(--COM1.RX_TimeOut == 0)
+			{
+				if(COM1.RX_Cnt > 0)
+				{
+					for(i=0; i<COM1.RX_Cnt; i++)	TX1_write2buff(RX1_Buffer[i]);	//收到的数据原样返回
+				}
+				COM1.RX_Cnt = 0;
+			}
+		}
+	}
+}
+
+
+
